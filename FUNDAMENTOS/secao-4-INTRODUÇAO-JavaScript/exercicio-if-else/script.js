70a51d3aab9e561e8c0594ef25647a321317e6bf
Vinícius Souza e Santos <117941548+viniciussouzas@users.noreply.github.com>
2022-11-23 18:33:47 -0300

    Delete FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/exercicio-if-else directory

--- FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/exercicio-if-else/script.js
+++
@@ -1,9 +0,0 @@
-const scoreCandidate = 59
-
-if (scoreCandidate >= 80) {
-  console.log ("Parabéns, você foi aprovado(a)!");
-} else if (scoreCandidate <= 80 && scoreCandidate >= 60) {
-  console.log("Você está na nossa lista de espera!");
-} else {
-  console.log ("Você foi reprovado!");
-}
