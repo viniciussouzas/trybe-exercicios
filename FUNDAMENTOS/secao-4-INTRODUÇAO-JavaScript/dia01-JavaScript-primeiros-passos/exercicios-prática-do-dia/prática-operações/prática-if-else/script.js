cf503d9b9de978fd9cd26f62ead0d258d6d1f8ef
Vinícius Souza <tradutorvinicius@gmail.com>
2022-11-23 19:39:41 -0300

    Resolvendo exercicios-prática do dia: operações, e if else

---
+++ FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/dia01-JavaScript-primeiros-passos/exercicios-prática-do-dia/prática-operações/prática-if-else/script.js
@@ -0,0 +1,32 @@
+const a = 2;
+const b = 5;
+
+if (a > b) {
+console.log("2 é maior que 5");
+} else {
+  console.log("5 é maior que 2");
+};
+
+const c = 4;
+const d = 7;
+const e = 8;
+
+if (c > d && c > e) {
+  console.log("4 é o maior dos três números");
+} else if (d > c && d > e) {
+  console.log("7 é o maior dos três números");
+} else {
+  console.log("8 é o maior dos três números");
+}
+
+let value = 0
+
+if (value > 0) {
+  console.log("positive");
+} else if (value < 0) {
+  console.log("negative");
+} else {
+  console.log("zero");
+}
+
+
---
+++ FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/dia01-JavaScript-primeiros-passos/exercicios-prática-do-dia/prática-operações/script.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 3;
+
+let adition = a + b;
+console.log('Soma: ' + adition);
+
+let subtraction = a - b;
+console.log('Subtração: ' + subtraction);
+
+let multiplication = a * b;
+console.log ("Multiplicação: " + multiplication);
+
+let division = a / b;
+console.log("Divisão: " + division);
+
+let rest = a % b;
+console.log("Módulo: " + rest);
