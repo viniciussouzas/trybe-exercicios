5e9dd84a059d2a6c9f400a04ac01bd00173aa17b
Vinícius Souza <tradutorvinicius@gmail.com>
2022-11-30 18:38:31 -0300

    Criado arquivo para resolver exercicio 1 de práticas sugeridas para fixar funções

---
+++ FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/dia04-JavaScript-Objetos-Funcoes/práticas-sugeridas/functions.js
@@ -0,0 +1,26 @@
+// primeiro-exercício
+
+const a = 6;
+const b = 8;
+
+function sum () {
+  return a + b;
+};
+
+function subtraction () {
+  return a - b;
+};
+
+function multiplication () {
+  return a * b;
+};
+
+function division () {
+  return a / b;
+};
+
+function module () {
+  return a % b;
+};
+
+console.log("Soma de a + b = " + sum() + "; " + "Subtração de a - b = " + subtraction() + "; " + "Multiplicação de a * b = " + multiplication() + "; " + "Divisão de a / b = " + division() + "; " + "Módulo de a % b = " + module() + "; ");
