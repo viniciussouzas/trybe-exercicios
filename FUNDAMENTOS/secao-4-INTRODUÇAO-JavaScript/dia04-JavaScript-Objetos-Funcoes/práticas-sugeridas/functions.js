8e4b723c8b6183095eddb20296289ee9c8bd69c9
Vinícius Souza <tradutorvinicius@gmail.com>
2022-11-30 18:49:33 -0300

    Resolvendo segundo exercício da prática sugerida sobre funções

--- FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/dia04-JavaScript-Objetos-Funcoes/práticas-sugeridas/functions.js
+++ FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/dia04-JavaScript-Objetos-Funcoes/práticas-sugeridas/functions.js
@@ -24,3 +24,22 @@ function module () {
 };
 
 console.log("Soma de a + b = " + sum() + "; " + "Subtração de a - b = " + subtraction() + "; " + "Multiplicação de a * b = " + multiplication() + "; " + "Divisão de a / b = " + division() + "; " + "Módulo de a % b = " + module() + "; ");
+
+console.log('***********');
+
+const number1 = 37;
+const number2 = 20;
+
+function biggest () {
+  if (number1 > number2) {
+    return number1 + " é maior que " + number2 + "!";
+  } else if (number2 > number1) {
+    return number2 + " é maior que " + number1 + "!";
+  } else {
+    return number1 + " e " + number2 + " são iguais!";
+  }
+}
+
+console.log(biggest());
+
+console.log("***************");
