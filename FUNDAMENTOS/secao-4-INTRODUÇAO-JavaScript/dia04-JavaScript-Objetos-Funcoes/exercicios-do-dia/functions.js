4b3016b49409ab9dc9e56ee9f74e486a8e964d55
Vinícius Souza <tradutorvinicius@gmail.com>
2022-12-01 12:02:07 -0300

    Resolvendo exercicio do dia sobre funções descobrindo o maior nome do array

--- FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/dia04-JavaScript-Objetos-Funcoes/exercicios-do-dia/functions.js
+++ FUNDAMENTOS/secao-4-INTRODUÇAO-JavaScript/dia04-JavaScript-Objetos-Funcoes/exercicios-do-dia/functions.js
@@ -1,3 +1,5 @@
+// palindromo-exercicio
+
 function verifyPalindrome (word) {
   for (let index in word) {
     if (word[index] === word[(word.length - 1) - index]) {
@@ -6,20 +8,56 @@ function verifyPalindrome (word) {
        return false;
     }
   }
-}
+};
 
 console.log(verifyPalindrome('xablau'));
 
 console.log("**********");
 
+// index do maior valor do array - exercicio
+
 function indexOfbiggestValue (arrays) {
-  let indexOfValue = 0;
+  let indexOfBiggest = 0;
   for (let index in arrays) {
-    if (arrays[indexOfValue] < arrays[index]) {
-      indexOfValue = index;
+    if (arrays[indexOfBiggest] < arrays[index]) {
+      indexOfBiggest = index;
     }
   }
-  return indexOfValue;
-}
+  return indexOfBiggest;
+};
 
 console.log(indexOfbiggestValue([2, 10, 37, 11, 28]));
+
+console.log("************");
+
+// index do menor valor do array - exercicio
+
+function indexOfSmallestValue (arrays) {
+  let indexOfSmallest = 0;
+  for (let index in arrays) {
+    if (arrays[indexOfSmallest] > arrays[index]) {
+      indexOfSmallest = index;
+    }
+  }
+  return indexOfSmallest;
+}
+
+console.log(indexOfSmallestValue([12, 45, 23, 49, 7, 21]));
+
+console.log("************");
+
+// nome com a maior quantidade de caracteres - exercicio
+
+ function biggestName (names) {
+  let mostCaracters = names[0];
+  for (let caracters in names) {
+    if (mostCaracters.length < names[caracters].length) {
+      mostCaracters = names[caracters];
+    }
+  }
+  return mostCaracters;
+ }
+
+ console.log(biggestName(['Ana', 'João', 'Vinícius', 'Felipe']));
+
+ console.log("**********");
