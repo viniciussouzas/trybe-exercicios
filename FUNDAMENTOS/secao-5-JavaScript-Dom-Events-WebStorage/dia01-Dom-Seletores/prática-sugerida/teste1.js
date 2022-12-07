db451f251814d55837ee6ca4a7c7d49aa67e6e8d
Vinícius Souza <tradutorvinicius@gmail.com>
2022-12-07 18:24:54 -0300

    Resolvendo prática sugerida no conteúdo do dia, alterando título, subtítulo e parágrafo da página

---
+++ FUNDAMENTOS/secao-5-JavaScript-Dom-Events-WebStorage/dia01-Dom-Seletores/prática-sugerida/teste1.js
@@ -0,0 +1,10 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const title = document.
+getElementById('page-title');
+title.innerText = 'HP';
+
+document.getElementById('second-paragraph').innerText = 'Isso é apenas um teste';
+
+document.getElementById('subtitle').innerText = 'Testando';
