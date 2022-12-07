7e4c0af696df84bedd3e9f09f0ceac09d86cb8a4
Vinícius Souza <tradutorvinicius@gmail.com>
2022-12-07 18:47:37 -0300

    Adiciona mais práticas sugeridas no conteúdo do dia, class para 2 paragrafos, recuperando a class e alterando estilo, recuperando subtítulo através de sua tag e alterando estilo

--- FUNDAMENTOS/secao-5-JavaScript-Dom-Events-WebStorage/dia01-Dom-Seletores/prática-sugerida/teste1.js
+++ FUNDAMENTOS/secao-5-JavaScript-Dom-Events-WebStorage/dia01-Dom-Seletores/prática-sugerida/teste1.js
@@ -8,3 +8,7 @@ title.innerText = 'HP';
 document.getElementById('second-paragraph').innerText = 'Isso é apenas um teste';
 
 document.getElementById('subtitle').innerText = 'Testando';
+
+document.getElementsByClassName('paragraphs')[0].style.color='purple';
+
+document.getElementsByTagName('h4')[0].style.color='blue'
