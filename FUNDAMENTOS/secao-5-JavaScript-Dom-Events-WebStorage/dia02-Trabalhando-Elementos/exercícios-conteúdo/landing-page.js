cdb1e4cfd53e1b7ad5716c97e1b92c9e7c073f65
Vinícius Souza <tradutorvinicius@gmail.com>
2022-12-08 19:04:04 -0300

    Exercícios do dia: adiciona section com class center-content; e adiciona tag p com texto

--- FUNDAMENTOS/secao-5-JavaScript-Dom-Events-WebStorage/dia02-Trabalhando-Elementos/exercícios-conteúdo/landing-page.js
+++ FUNDAMENTOS/secao-5-JavaScript-Dom-Events-WebStorage/dia02-Trabalhando-Elementos/exercícios-conteúdo/landing-page.js
@@ -1,12 +1,35 @@
 const body = document.getElementById('body');
 
+//
+
 const createH1 = document.createElement('h1');
 createH1.id = 'title';
 createH1.innerText = 'TrybeTrip - Agência de Viagens';
 
 body.appendChild(createH1);
 
+//
+
 const createMain = document.createElement('main');
 createMain.className = 'main-content';
 
 body.appendChild(createMain);
+
+//
+
+const main = document.getElementsByClassName('main-content')[0];
+
+const createSectionCenter = document.createElement('section');
+createSectionCenter.className = 'center-content';
+
+main.appendChild(createSectionCenter);
+
+//
+
+const sectionCenter = document.getElementsByClassName('center-content')[0];
+
+const createPOfSectionCenter = document.createElement('p');
+createPOfSectionCenter.id = 'paragraph-of-sectionCenter';
+createPOfSectionCenter.innerText = 'Aqui temos o texto do SectionCenter';
+
+sectionCenter.appendChild(createPOfSectionCenter);
