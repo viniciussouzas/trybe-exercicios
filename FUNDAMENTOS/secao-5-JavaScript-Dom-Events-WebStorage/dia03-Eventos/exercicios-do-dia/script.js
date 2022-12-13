4d40887f4347885fae93bcd6761e20db3454b840
Vinícius Souza <tradutorvinicius@gmail.com>
2022-12-13 12:04:19 -0300

    Resolvendo exercicios do dia: cria dias do calendário dentro da ul-days como li's de classe day

--- FUNDAMENTOS/secao-5-JavaScript-Dom-Events-WebStorage/dia03-Eventos/exercicios-do-dia/script.js
+++ FUNDAMENTOS/secao-5-JavaScript-Dom-Events-WebStorage/dia03-Eventos/exercicios-do-dia/script.js
@@ -14,3 +14,19 @@ const createDaysOfTheWeek = () => {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const createDaysOfTheMonth = () => {
+  const decemberDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const decemberDaysList = document.getElementById('days');
+
+  for (let index = 0; index < decemberDays.length; index +=1) {
+    const numberDays = decemberDays[index];
+    const numberDaysListItem = document.createElement('li');
+    numberDaysListItem.className = 'day';
+    numberDaysListItem.innerHTML = numberDays;
+
+    decemberDaysList.appendChild(numberDaysListItem);
+  };
+}
+
+createDaysOfTheMonth();
