6fedf2e58df154bb4c08c52f31d6456143f352b6
Vinícius Souza <tradutorvinicius@gmail.com>
2023-01-18 15:22:09 -0300

    Resolve prática sugerida sobre objects keys, values e entries

---
+++ FUNDAMENTOS/secao-6-Intro-JavaScriptES6-Testes-Unitários/dia01-fluxo-de-exceções-E-manipulação-de-objetos/prática-sugerida/object-entries.js
@@ -0,0 +1,14 @@
+const countries = {
+  franca: 'Paris',
+  brasil: 'Brasília',
+  espanha: 'Madrid',
+  portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(countries);
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
