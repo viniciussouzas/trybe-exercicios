5221a37ea3027c9c9f9e1abe69a0cdf2c44ac711
Vinícius Souza <tradutorvinicius@gmail.com>
2023-01-31 14:36:57 -0300

    Resolve exercicio do dia, exercise 3

---
+++ FUNDAMENTOS/secao-7-Higher-Order-Functions-JavaScriptES6/dia05-Array-Destruc-Default-Destruc-Object-Property-Shorthand-Default-Parameters/exercicios-do-dia/exercise3.js
@@ -0,0 +1,6 @@
+const greet = (name, greeting = 'Hi') => `${greeting}, ${name}!`;
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
