2ef0dda0d26f19fbf3428611305986a1fdf581c8
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-02 15:15:26 -0300

    Resolve exercicio de fixação fancy buttons state vs props

--- FRONT-END/secao-3-Componentes-Estado-Eventos-Forms/dia01-Componentes-Estado-Eventos/fancy-buttons/src/App.js
+++ FRONT-END/secao-3-Componentes-Estado-Eventos-Forms/dia01-Componentes-Estado-Eventos/fancy-buttons/src/App.js
@@ -26,35 +26,43 @@ class App extends Component {
   // };
 
   button1 = () => {
+    const { counter1 } = this.state
+
     this.setState((previousState, _props) => ({
       counter1: previousState.counter1 += 1
-    }));
+    }), () => console.log(`Botão 1 ${this.verifyIsEven(counter1)}`));
   };
   
   button2 = () => {
+    const { counter2 } = this.state;
+
      this.setState((previousState, _props) => ({
       counter2: previousState.counter2 + 1
-     }));
+     }), () => console.log(`Botão 2 ${this.verifyIsEven(counter2)}`));
   };
 
   button3 () {
+    const { counter3 } = this.state;
+
     this.setState((previousState, _props) => ({
       counter3: previousState.counter3 + 1
-    }));
+    }), () => console.log(`Botão 3 ${this.verifyIsEven(counter3)}`));
   };
+
+  verifyIsEven = (counter) => counter % 2 === 0 ? 'green' : 'red';
 
   render() {
     const { counter1, counter2, counter3 } = this.state;
 
     return (
       <div>
-        <button onClick={this.button1}>
+        <button onClick={this.button1} style={ { backgroundColor: this.verifyIsEven(counter1) } }>
           { counter1 }
         </button>
-        <button onClick={this.button2}>
+        <button onClick={this.button2} style={ { backgroundColor: this.verifyIsEven(counter2) } }>
           { counter2 }
         </button>
-        <button onClick={this.button3}>
+        <button onClick={this.button3} style={ { backgroundColor: this.verifyIsEven(counter3) } }>
           { counter3 }
         </button>
       </div>
