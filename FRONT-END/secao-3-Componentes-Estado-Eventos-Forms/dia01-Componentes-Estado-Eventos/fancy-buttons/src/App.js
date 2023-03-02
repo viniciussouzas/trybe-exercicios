82b4f16e76ca31c91a3128479cd9bdb85b9533df
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-02 12:24:56 -0300

    Resolve prática sugerida fancy buttons eventos de forma apropriada (utilizando arrow functions e a sintaxe do constructor ao utilizar funções normais)

--- FRONT-END/secao-3-Componentes-Estado-Eventos-Forms/dia01-Componentes-Estado-Eventos/fancy-buttons/src/App.js
+++ FRONT-END/secao-3-Componentes-Estado-Eventos-Forms/dia01-Componentes-Estado-Eventos/fancy-buttons/src/App.js
@@ -1,23 +1,37 @@
 import { Component } from 'react';
 import './App.css';
 
-const button1 = () => console.log("Clicou botão 1");
-
-const button2 = () => console.log("Clicou botão 2");
-
-const button3 = () => console.log("Clicou botão 3");
+// Abaixo realizei as duas formas de vincular funções nas classes... Com arrow function, não se faz necessário utilizar o constructor. Já com funções normais, é preciso utilizar o constructor, pois senão o this não é reconhecido dentro da nossa função e retorna undefined
 
 class App extends Component {
+  constructor() {
+    super()
+    
+    this.button3 = this.button3.bind(this);
+  }
+
+  button1 = () => console.log("Clicou botão 1");
+  
+  button2 = () => {
+      console.log(this)
+      console.log("Clicou botão 2");
+  }
+
+  button3 () {
+    console.log(this);
+    console.log("Clicou botão 3");
+  }
+  
   render() {
     return (
       <div>
-        <button onClick={button1}>
+        <button onClick={this.button1}>
           My button1
         </button>
-        <button onClick={button2}>
+        <button onClick={this.button2}>
           My button2
         </button>
-        <button onClick={button3}>
+        <button onClick={this.button3}>
           My button3
         </button>
       </div>
