79575f8500299e03baeceda64a51d2dbc238fb31
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-03 12:24:07 -0300

    Resolve prática sugerida my-form-1.0 cria event handler genérico

--- FRONT-END/secao-3-Componentes-Estado-Eventos-Forms/dia02-Formularios-React/my-form-1.0/src/Form.js
+++ FRONT-END/secao-3-Componentes-Estado-Eventos-Forms/dia02-Formularios-React/my-form-1.0/src/Form.js
@@ -2,30 +2,38 @@ import { Component } from 'react';
 
 class Form extends Component {
   state = {
-    description: '',
+    select: '',
+    nome: '',
+    check: false,
+    description: ''
   }
 
-  handleDescription = (event) => this.setState(({
-    description: event.target.value
-  }));
+  handlerGlobal = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+  
+    this.setState(({
+      [ name ]: value,
+    }));
+  }
 
   render() {
     return (
       <form className="form">
         <label>
           Selecione seu Estado:
-          <select>
+          <select name="select" value={this.state.select} onChange={this.handlerGlobal}>
             <option>option1</option>
             <option>option2</option>
           </select>
         </label>
 
-        <input name="nome" type="text"/>
-        <input name="check" type="checkbox"/>
+        <input name="nome" type="text" value={this.state.nome} onChange={this.handlerGlobal}/>
+        <input name="check" type="checkbox" value={this.state.check} onChange={this.handlerGlobal}/>
 
         <label>
           Adicione uma breve descrição sobre você:
-          <textarea name="description" value={this.state.description} onChange={this.handleDescription}/>
+          <textarea name="description" value={this.state.description} onChange={this.handlerGlobal}/>
         </label>
       </form>
     )
