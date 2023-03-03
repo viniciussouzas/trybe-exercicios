56b42f17aeee9016e26c5dcef7aefd9401a67f0e
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-03 12:50:39 -0300

    Adiciona fieldset sugerido na prática

--- FRONT-END/secao-3-Componentes-Estado-Eventos-Forms/dia02-Formularios-React/my-form-1.0/src/Form.js
+++ FRONT-END/secao-3-Componentes-Estado-Eventos-Forms/dia02-Formularios-React/my-form-1.0/src/Form.js
@@ -20,21 +20,23 @@ class Form extends Component {
   render() {
     return (
       <form className="form">
-        <label>
-          Selecione seu Estado:
-          <select name="select" value={this.state.select} onChange={this.handlerGlobal}>
-            <option>option1</option>
-            <option>option2</option>
-          </select>
-        </label>
+        <fieldset>
+          <label>
+            Selecione seu Estado:
+            <select name="select" value={this.state.select} onChange={this.handlerGlobal}>
+              <option>option1</option>
+              <option>option2</option>
+            </select>
+          </label>
 
-        <input name="nome" type="text" value={this.state.nome} onChange={this.handlerGlobal}/>
-        <input name="check" type="checkbox" value={this.state.check} onChange={this.handlerGlobal}/>
+          <input name="nome" type="text" value={this.state.nome} onChange={this.handlerGlobal}/>
+          <input name="check" type="checkbox" value={this.state.check} onChange={this.handlerGlobal}/>
 
-        <label>
-          Adicione uma breve descrição sobre você:
-          <textarea name="description" value={this.state.description} onChange={this.handlerGlobal}/>
-        </label>
+          <label>
+            Adicione uma breve descrição sobre você:
+            <textarea name="description" value={this.state.description} onChange={this.handlerGlobal}/>
+          </label>
+        </fieldset>
       </form>
     )
   }
