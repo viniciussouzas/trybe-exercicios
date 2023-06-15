d2437cd6c6e01e5038b49de90f7c754bea76b654
Vinícius Souza <tradutorvinicius@gmail.com>
2023-06-15 11:19:50 -0300

    Resolve exercícios do dia

--- BACK-END/secao-4-Intro-Node.js/dia03-Node.js-Testes-de-Integração/cacau-trybe/tests/integration/chocolates.test.js
+++ BACK-END/secao-4-Intro-Node.js/dia03-Node.js-Testes-de-Integração/cacau-trybe/tests/integration/chocolates.test.js
@@ -127,4 +127,13 @@ describe('Testando a API Cacau Trybe', function () {
       ]);
     });
   });
+
+  describe('Usando o método GET em /chocolates/total para retornar a quantidade total de chocolates', function () {
+    it('Retorna 4 chocolates no total', async function () {
+      const response = await chai.request(app).get('/chocolates/total');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal({ totalChocolates: 4});
+    });
+  });
 });
