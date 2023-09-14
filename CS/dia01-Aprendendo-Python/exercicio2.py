8c2088fe8dfe5b4a6cff185221726a8b7dc91e34
Vinícius Souza e Santos <117941548+viniciussouzas@users.noreply.github.com>
2023-09-14 12:42:23 -0300

    Delete CS/dia01-Aprendendo-Python directory

--- CS/dia01-Aprendendo-Python/exercicio2.py
+++
@@ -1,5 +0,0 @@
-def mean(numbers):
-    total = 0
-    for number in numbers:
-        total += number
-    return total / len(numbers)
--- CS/dia01-Aprendendo-Python/exercicio3.py
+++
@@ -1,7 +0,0 @@
-def draw_square(n):
-    for row in range(n):
-        print(n * '*')
-
-def draw_triangle(n):
-    for row in range(1, n + 1):
-        print(row * '*')
